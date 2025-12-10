976332f678f5fca963e554d193b4a1c04dc115b8
Artur Z <arturziianbaev@gmail.com>
2025-12-10 15:26:29 -0800

    responsive nav

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './NavBar.css'
 import { Link } from 'react-router-dom'
 type Props = {}
 
 function NavBar({}: Props) {
+  const [open, setOpen] = useState<boolean>(false);
   return (
     <nav>
-      <Link to='/' id='nav_primary'>AZ <span id='nav_secondary'>language school</span></Link>
-      <div>
+      <Link to='/' id='nav_primary'>AZ <span id='nav_secondary' >language school</span></Link>
+      <div className={`nav-links ${open? 'open' : ''}`} onClickCapture={()=>{setOpen(v=>!v)}}>
+        <Link to='/'              className='lTd' id='nav_home'>Home</Link>
         <Link to='/kids'          className='lTd'>Kids</Link>
         <Link to='/consultation'  className='lTd' id='nav_consult'>Free consultation</Link>
         <Link to="/login"         className='lTd'>Login</Link>
       </div>
+      <button className="nav-toggle" aria-expanded={open} onClick={()=>{setOpen(v=>!v)}}>=</button>
     </nav>
   )
 }
