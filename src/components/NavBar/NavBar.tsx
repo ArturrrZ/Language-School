e9ce845ff83803dc3ff915df6c1cd984b18400c1
Artur Z <arturziianbaev@gmail.com>
2025-12-15 13:15:58 -0800

    refactor: update NavBar structure and styles; adjust main layout for consistency

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -34,25 +34,27 @@ function NavBar() {
   }, [location.pathname]);
   return (
     <nav>
-      <Link to='/' id='nav_primary' onClick={closeMenu}>AZ <span id='nav_secondary'>language school</span></Link>
+      <div className="nav-inner">
+        <Link to='/' id='nav_primary' onClick={closeMenu}>AZ <span id='nav_secondary'>language school</span></Link>
 
-      {/* Desktop + Mobile links */}
-      <div className={`nav-links ${open ? 'open' : ''}`}>
-        <Link to='/'              className='lTd' id='nav_home' onClick={closeMenu}>Home</Link>
-        <Link to='/kids'          className='lTd' onClick={closeMenu}>Kids</Link>
-        <Link to='/consultation'  className='lTd' id='nav_consult' onClick={closeMenu}>Free consultation</Link>
-        <Link to='/login'         className='lTd' onClick={closeMenu}>Login</Link>
-      </div>
+        {/* Desktop + Mobile links */}
+        <div className={`nav-links ${open ? 'open' : ''}`}>
+          <Link to='/'              className='lTd' id='nav_home' onClick={closeMenu}>Home</Link>
+          <Link to='/kids'          className='lTd' onClick={closeMenu}>Kids</Link>
+          <Link to='/consultation'  className='lTd' id='nav_consult' onClick={closeMenu}>Free consultation</Link>
+          <Link to='/login'         className='lTd' onClick={closeMenu}>Login</Link>
+        </div>
 
-      {/* Mobile toggle */}
-      <button 
-        className="nav-toggle" 
-        aria-expanded={open} 
+        {/* Mobile toggle */}
+        <button 
+          className="nav-toggle" 
+          aria-expanded={open} 
         aria-label="Toggle menu"
         onClick={() => setOpen(!open)}
       >
       <MenuIcon />
       </button>
+      </div>
     </nav>
   )
 }
