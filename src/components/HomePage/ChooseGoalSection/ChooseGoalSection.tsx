9e2bbc035b1973e0e25621ae8f7936f62ab7527b
Artur Z <arturziianbaev@gmail.com>
2025-12-16 15:11:43 -0800

    Add ChooseGoalSection component with styling and functionality
    - Created ChooseGoalSection component to allow users to select learning goals.
    - Implemented a list of goals with corresponding descriptions.
    - Added CSS for styling the section, including responsive design.
    - Integrated an image to enhance the visual appeal of the section.

---
+++ src/components/HomePage/ChooseGoalSection/ChooseGoalSection.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import './ChooseGoalSection.css'
+import cubeAndCursor from '../../../assets/cubeAndCursor.avif'
+
+type ApplyType = {
+  display: boolean;
+  title: string;
+  text: string;
+};
+
+type Props = {
+  setApply: (value: ApplyType) => void;
+}
+
+const goals = [
+  {title:'Education', text:'We prepare you for admission to foreign universities and participation in international exchange programs.'},
+  {title:'Job', text:'We help you master business English tailored to your profession and your current needs.'},
+  {title:'Traveling', text:'We teach you useful phrases and skills so you can confidently communicate anywhere in the world.'},
+  {title:'Movies and TV', text:'We help you understand movies and TV series in English, enjoying the original voices of the actors.'},
+  {title:'Personal', text:'We create a personalized learning program based on your goals and requests.'},
+]
+
+function ChooseGoalSection({setApply}: Props) {
+  return (
+    <section id='choose-goal-section'>
+      <div className="choose-goal-left">
+        <h2 className="choose-goal-section-header">Choose a goal - we'll find the best solution!</h2>
+        <div className="choose-goal-list">
+          {goals.map((g, i) => (
+           <div onClick={()=>{setApply({display:true, title:g.title, text:g.text})}} className="choose-goal-box" id={g.title} key={g.title + i}>
+             <div className="choose-goal-title">{g.title}</div>
+            </div>
+         ))}
+        </div>
+      </div>
+      <img className="choose-goal-img" src={cubeAndCursor} alt='cube and cursor' />
+
+    </section>
+  )
+}
+
+export default ChooseGoalSection
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,31 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Greeting from '../components/HomePage/Greeting/Greeting';
 import GroupSection from '../components/HomePage/GroupSection/GroupSection';
+import ChooseGoalSection from '../components/HomePage/ChooseGoalSection/ChooseGoalSection';
 
 type Props = {}
 
+type ApplyType = {
+  display: boolean;
+  title: string;
+  text: string;
+};
+
 function HomePage({}: Props) {
+  const [apply, setApply] = useState<ApplyType>({
+    display:false,
+    title:'Test title',
+    text: 'test text text text'
+  })
   return (
     <>
       <Greeting />
       <GroupSection />
+      <ChooseGoalSection setApply={setApply}/>
+      <div id="apply">
+        <h3>{apply.title}</h3>
+        <p>{apply.text}</p>
+      </div>
     </>
   )
 }
