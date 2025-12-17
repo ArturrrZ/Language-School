1f666d212a54726c8915e273c9fdf18ce9578a23
Artur Z <arturziianbaev@gmail.com>
2025-12-16 19:58:58 -0800

    feat: enhance ApplyFixed component with form inputs and submission handling

--- src/components/HomePage/ApplyFixed/ApplyFixed.tsx
+++ src/components/HomePage/ApplyFixed/ApplyFixed.tsx
@@ -4,13 +4,31 @@ import './ApplyFixed.css'
 import CancelOutlinedIcon from '@mui/icons-material/CancelOutlined';
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
+import { Button, Checkbox, FormControlLabel, TextField } from '@mui/material';
+import TelegramIcon from '@mui/icons-material/Telegram'
+
+
 type Props = {
     apply: ApplyType,
     setApply: (value: ApplyType) => void;
 }
 
 function ApplyFixed({apply,setApply}: Props) {
-    const mainRef = useRef<HTMLDivElement>(null)
+    const mainRef = useRef<HTMLFormElement>(null)
+    const [terms, setTerms] = React.useState(false)
+    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault()
+        alert('Send POST request')
+    
+        // reset native inputs
+        e.currentTarget.reset()
+    
+        // reset controlled checkboxes
+        setTerms(false)
+      }
+      function handleClickButton() {
+        alert('Redirect to Telegram')
+    }
     useEffect(() => {
       function handleOutsideClick(e: MouseEvent) {
         if (mainRef.current && !mainRef.current.contains(e.target as Node)) {
@@ -30,17 +48,67 @@ function ApplyFixed({apply,setApply}: Props) {
     }, [apply, setApply]);
 
   if (!apply.display) return null;
+
   return (
     <div className="apply-fixed open">
-      <div className="apply-main" ref={mainRef}>
+      <form className="apply-main" ref={mainRef} onSubmit={handleSubmit}>
         <div className="apply-top">
-          <h2>{apply.title}</h2>
+          <h2 className='apply-title'>English for {apply.title==='Personal'&&' any '}{apply.title}{apply.title==='Personal'&&' reason'}</h2>
           <IconButton aria-label="close" onClick={() => { setApply({ ...apply, display: false }) }}>
             <CloseIcon />
           </IconButton>
         </div>
         <p>{apply.text}</p>
-      </div>
+        <TextField
+          required
+          id="outlined-basic"
+          label="Your name"
+          variant="outlined"
+        />
+        <TextField
+          required
+          id="outlined-basic"
+          label="Your phone number"
+          variant="outlined"
+        />
+        <TextField
+          required
+          type='email'
+          id="outlined-basic"
+          label="Your email"
+          variant="outlined"
+        />
+        <FormControlLabel
+          id='home_form_terms_text'
+          required
+          control={
+            <Checkbox
+              color='default'
+              checked={terms}
+              onChange={(e) => setTerms(e.target.checked)}
+            />
+          }
+          label="I agree with terms and conditions"
+        />
+        <Button
+          type='submit'
+          variant="contained"
+          sx={{ height: '56px', maxWidth: '1000px', width: '100%' }}
+          className="custom-button"
+        >
+          Apply
+        </Button>
+        <Button
+          type="button"
+          onClick={handleClickButton}
+          variant="outlined"
+          sx={{ height: '56px', maxWidth: '1000px', width: '100%' }}
+          className="custom-button"
+          startIcon={<TelegramIcon />}
+        >
+          Telegram
+        </Button>
+      </form>
     </div>
   )
 }
